a3f07b28cfbc8f75499c5686390dad3ebe7943be
Shreeman Gokul Hariharan <sghariha@gmail.com>
2019-07-19 19:40:54 -0700

    Express setup; dependencies installed w npm

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://sghariha:sghariha@cluster0-fvvaf.mongodb.net/test?retryWrites=true",
-  secretOrKey: "secret"
-};
--- server.js
+++ server.js
@@ -1,36 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
-
+const express = require('express');
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// mongodb config
-const db = require("./config/keys").mongoURI;
-// connect to mongodb
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to mongoDB."))
-  .catch(err => console.log(err));
-
-//passport middleware
-app.use(passport.initialize());
-
-//passport config
-require("./config/passport")(passport);
-
-// use routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}.`));
+app.get('/', (req, res) => res.send('API running'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
